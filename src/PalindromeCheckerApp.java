e47c70900426cff7df1c9413e2438a747270d350
shresth-stack <shresthsehgal11@gmail.com>
2026-03-10 19:44:08 +0530

    UC2: Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -4,6 +4,20 @@ public class PalindromeCheckerApp {
 
         System.out.println("Welcome to Palindrome Checker Application");
 
+        String word = "madam";
+
+        String reversed = "";
+
+        for(int i = word.length() - 1; i >= 0; i--){
+            reversed = reversed + word.charAt(i);
+        }
+
+        if(word.equals(reversed)){
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+
     }
 
 }
